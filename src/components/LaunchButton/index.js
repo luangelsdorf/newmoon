7a7612b357fa23ad014e4139e842a188d3862531
lulaluano <luanferreira2136@gmail.com>
2021-06-14 21:23:00 -0300

    more responsiveness adjustments

--- src/components/LaunchButton/index.js
+++ src/components/LaunchButton/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import styles from './LaunchButton.module.css'
+import styles from './LaunchButton.module.css';
 
 export default function LaunchButton(props) {
+
+  function handleClick() {
+    document.body.style.backgroundPositionY = 'top';
+  }
+
   return (
-    <button className={`${styles.launch}`}>{props.children}</button>
+    <button onClick={handleClick} className={`${styles.launch}`}>{props.children}</button>
   )
 }
--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -4,13 +4,13 @@ import styles from './MainSection.module.css'
 export default function Main(props) {
 
   let d;
-  props.form ? d = 'none' : d = 'block';
+  props.form ? d = 'none' : d = 'flex';
 
   return (
     <>
       <section className={`${styles.section} container-fluid`} style={{display: d}}>
         <div className="row w-100 flex-center text-center">
-          <div className="col-4">
+          <div className="col-12 col-md-8">
             {props.children}
           </div>
         </div>
