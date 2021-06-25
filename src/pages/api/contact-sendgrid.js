603109439e668d38a95c97e70e6d5e018113eb82
lulaluano <luanferreira2136@gmail.com>
2021-06-25 15:17:17 -0300

    SendGrid test

--- src/pages/api/contact-sendgrid.js
+++ src/pages/api/contact-sendgrid.js
@@ -2,16 +2,19 @@ export default function (req, res) {
   const sgMail = require('@sendgrid/mail')
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
-    to: 'luangelsdorf@gmail.com', // Change to your recipient
-    from: 'luanferreira2136@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js.',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    to: 'luanferreira2136@gmail.com',
+    from: 'newmoon.sender@gmail.com',
+    subject: 'Contato site NewMoon',
+    text: `Novo e-mail: ${req.body.email}`,
+    html: `
+      <h3>Novo e-mail recebido:</h3>
+      <code>${req.body.email}</code>
+          `,
   }
   sgMail
     .send(msg)
     .then(() => {
-      console.log('Email sent')
+      console.log('Email sent');
     })
     .catch((error) => {
       console.error(error)
